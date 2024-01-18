cf9baf0f7bff500bdf6dd16f67149df5eeef3ea1
ninifutkaradze <ninifutkaradze51@gmail.com>
2024-01-18 16:16:06 +0400

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,44 @@
+console.log("JS homework!");
+//Task1-simple variables//
+let name="Nini";
+let surname="Phutkaradze";
+let age="20";
+let isMarried="null";
+isMarried=false;
+var isStudent=true;
+
+//Task2-math operations//
+let x=10;
+let y=15;
+let z=x+y;
+console.log(z);
+
+
+let a=50;
+let b=100;
+let c=a+b;
+console.log(c);
+
+
+let n=100;
+let m=500;
+let k=m*n;
+console.log(k);
+
+
+let o=81;
+let d=9;
+let j=o/d;
+console.log(j);
+
+
+//Task3-string concatenating//
+let sentance="I am"+name+"and I am"+age+"yeard old";
+let sentance2=`I am ${name} and I am ${age}years old`
+
+
+
+//Task4//
+const length=7;
+const width=5;
+const area=7*5;
